9649899d97ca360e7fb4690aee824355a731dbed
Estefania Cabrera <estefaniacabrera13@gmail.com>
2021-11-06 13:51:00 -0300

    primer entrega

---
+++ hoyo_en_uno/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hoyo en Uno Golf
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ir a mi tienda
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
